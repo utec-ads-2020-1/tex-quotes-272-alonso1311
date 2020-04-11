81b7216a5e448e8e9ab253afd8f3aa24d0a84afd
alonso1311 <alonso.barrios@utec.edu.pe>
2020-04-10 19:33:53 -0500

    finish and generalized

--- main.cpp
+++ main.cpp
@@ -3,25 +3,23 @@
 #include<vector>
 
 using namespace std;
+string to_search = "\"", first_change = "``", second_change = "''";
+bool first = true;
+
+void Replace(const int&, string&);
 
 int main() {
     string line;        
-    bool first = true;
-    char to_search = '"';
     vector<string> result;
 
     while(getline(cin, line, '\n') and !line.empty()){
         while(true){
             int found = line.find(to_search);
-            if(found != string::npos && first == true){
-                line.replace(found, 1, "``");
-                first = false;
-            } else if(found != string::npos && first == false){
-                line.replace(found, 1, "''");
-                first = true;
-            } else {
+            if(found != string::npos)
+                Replace(found, line);
+            else 
                 break;
-            }
+            
         }
         result.push_back(line); 
     }
@@ -30,4 +28,14 @@ int main() {
         cout << i << endl;
 
     return 0;
+}
+
+void Replace(const int& found, string& line){
+    if(first == true){
+        line.replace(found, to_search.size(), first_change);
+        first = false;
+    } else if(first == false){
+        line.replace(found, to_search.size(), second_change);
+        first = true;
+    } 
 }
